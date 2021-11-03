8b7da6a988796adeffffcbd946a3c95d2fed991d
Arruza17 <ander.arruza@gmail.com>
2021-11-03 21:39:32 +0100

    Fixes in the UI, trying new components (fail). Minimalist design added

--- app/src/main/java/com/example/pufferfishminesweeper/MainActivity.java
+++ app/src/main/java/com/example/pufferfishminesweeper/MainActivity.java
@@ -7,11 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity {
 
     private Intent intent;
     private EditText editText_Nombre;
@@ -20,43 +21,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button buttonHelp;
     private Button buttonScoreBoard;
     private String choosedDifficulty = null;
+    private ImageButton logoChange;
+    private int easterEgg = 10;
+    private boolean cambioImagen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //OBTENCIÓN DE OBJECTOS
+        //OBTENCIÓN DE OBJETOS
         editText_Nombre = findViewById(R.id.editText_Nombre);
         radioGroupDifficulties = findViewById(R.id.radioGroupDifficulties);
         buttonPlay = findViewById(R.id.buttonPlay);
         buttonHelp = findViewById(R.id.buttonHelp);
         buttonScoreBoard = findViewById(R.id.buttonScoreBoard);
+        logoChange = findViewById(R.id.buttonChange);
         //AÑADIDO DE LISTENERS
-        buttonPlay.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.buttonPlay:
-                if(checkEveryField()){
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkEveryField()) {
                     generatePlayWindow();
                 }
-                break;
-            case R.id.buttonScoreBoard:
-                generateScoreBoardWindow();
-                break;
-            case R.id.buttonHelp:
-                generateHelpWindow();
-                break;
-
-        }
+            }
+        });
+        logoChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (easterEgg > 0) {
+                    if (cambioImagen) {
+                        logoChange.setBackgroundResource(R.drawable.pezglobo);
+                        cambioImagen = false;
+                    } else {
+                        logoChange.setBackgroundResource(R.drawable.pezglobovuelta);
+                        cambioImagen = true;
+                    }
+                    logoChange.setMaxWidth(75);
+                    logoChange.setMaxHeight(75);
+                    logoChange.setAdjustViewBounds(true);
+                    easterEgg--;
+                }
+            }
+        });
     }
+
 
     private boolean checkEveryField() {
         boolean ok = true;
-        if(editText_Nombre.getText().toString().isEmpty()){
-            Toast.makeText(getApplicationContext(),getString(R.string.escribeAqui),Toast.LENGTH_LONG).show();
+        if (editText_Nombre.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), getString(R.string.escribeAqui), Toast.LENGTH_LONG).show();
         }
         return ok;
     }
@@ -72,9 +85,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //startActivityForResult(intent, SECODNARY_ACTIVITY_1);
     }
 
-    @Override
-    public void onCheckedChanged(RadioGroup radioGroup, int i) {
-        RadioButton radioButtonCheck = findViewById(radioGroupDifficulties.getCheckedRadioButtonId());
-        choosedDifficulty = radioButtonCheck.getText().toString();
-    }
+
 }
