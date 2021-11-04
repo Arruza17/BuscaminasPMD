c6942ca540a70089080dd6a46e10dd39398851a8
Arruza17 <ander.arruza@gmail.com>
2021-11-04 23:49:40 +0100

    Arreglos casa

--- app/src/main/java/com/example/pufferfishminesweeper/MainActivity.java
+++ app/src/main/java/com/example/pufferfishminesweeper/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.pufferfishminesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,13 +17,18 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TableLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Intent intent;
+    boolean animation_Notdone = true;
+    private TableLayout tableLayout;
     private EditText editText_Nombre;
     private RadioGroup radioGroupDifficulties;
+    private TextView appName;
     private Button buttonPlay;
     private Button buttonHelp;
     private Button buttonScoreBoard;
@@ -30,10 +42,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //OBTENCIÓN DE OBJETOS
+        //tableLayout = findViewById(R.id.tableLayout);
         editText_Nombre = findViewById(R.id.editText_Nombre);
         radioGroupDifficulties = findViewById(R.id.radioGroupDifficulties);
         buttonPlay = findViewById(R.id.buttonPlay);
         buttonHelp = findViewById(R.id.buttonHelp);
+        appName = findViewById(R.id.appName);
         buttonScoreBoard = findViewById(R.id.buttonScoreBoard);
         logoChange = findViewById(R.id.buttonChange);
         //AÑADIDO DE LISTENERS
@@ -58,13 +72,34 @@ public class MainActivity extends AppCompatActivity {
                     }
                     logoChange.setMaxWidth(75);
                     logoChange.setMaxHeight(75);
-                    logoChange.setAdjustViewBounds(true);
                     easterEgg--;
+                } else {
+                    easterEgg();
                 }
             }
         });
     }
 
+    private void easterEgg() {
+        if (editText_Nombre.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), getString(R.string.introduceTextoPrimero), Toast.LENGTH_LONG).show();
+        } else {
+            if (animation_Notdone) {
+                animation_Notdone = false;
+                Toast.makeText(getApplicationContext(), getString(R.string.easterEggFound), Toast.LENGTH_LONG).show();
+                ObjectAnimator backgroundColorAnimator = ObjectAnimator.ofObject(appName,
+                        "textColor",
+                        new ArgbEvaluator(),
+                        Color.rgb(74, 67, 67),
+                        Color.rgb(239, 184, 16));
+                //2 second animation done
+                backgroundColorAnimator.setDuration(2000);
+                backgroundColorAnimator.start();
+                RadioButton extreme = new RadioButton(null);
+                radioGroupDifficulties.addView(extreme);
+            }
+        }
+    }
 
     private boolean checkEveryField() {
         boolean ok = true;
