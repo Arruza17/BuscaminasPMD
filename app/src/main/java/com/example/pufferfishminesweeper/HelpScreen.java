040cfcf617122c64959dd52fbb2e7c65c2cd194c
byrocket09 <83572660+byrocket09@users.noreply.github.com>
2021-11-11 16:42:21 +0100

    HelpScreen Finished.

--- app/src/main/java/com/example/pufferfishminesweeper/HelpScreen.java
+++ app/src/main/java/com/example/pufferfishminesweeper/HelpScreen.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.util.Locale;
@@ -15,7 +16,7 @@ import java.util.Locale;
 public class HelpScreen extends AppCompatActivity {
 
     private Button btn_Back;
-    private Button buttonTutorial;
+    private ImageButton buttonTutorial;
 
     private final String urlVideoCastellano = "https://www.youtube.com/watch?v=plNEIt9plzY&ab_channel=DaveT";
     private final String urlVideoIngles = "https://www.youtube.com/watch?v=7B85WbEiYf4&ab_channel=EricBuffington";
